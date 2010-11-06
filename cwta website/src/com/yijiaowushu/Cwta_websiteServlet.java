5cebb872563849009c0b56611db575b9921c8bae
gmli <gmli@D:gmliPROJECTStoodle>
2010-11-06 00:07:36 -0800

    Google App Engine-ified version of original website
    [git-p4: depot-paths = "//depot/": change = 76]

---
+++ cwta website/src/com/yijiaowushu/Cwta_websiteServlet.java
@@ -0,0 +1,13 @@
+package com.yijiaowushu;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Cwta_websiteServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
