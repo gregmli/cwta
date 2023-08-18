7249bbe21d0cfaaae25dde551bd7d34a19a44c2f
Gregory Li <gregmli@gmail.com>
2023-08-18 17:35:47 -0400

    Migrate to Python3
    Finally migrate to python3 version of AppEngine
    - required restructuring directories and renaming files
    - some core changes to main script
    - some updates also snuck in as I started updating before I realized I needed to do the migration

---
+++ redesign/website/main.py
@@ -0,0 +1,67 @@
+from flask import Flask, render_template, abort
+# from google.appengine.api import wrap_wsgi_app
+# from google.appengine.api import users
+# from google.appengine.ext import db
+
+
+# import jinja2
+# import os
+# import urllib
+from datetime import datetime
+
+# jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+
+# def urlencode_filter(s):
+#     if type(s) == 'Markup':
+#         s = s.unescape()
+#     s = s.encode('utf8')
+#     s = urllib.quote_plus(s)
+#     return jinja2.Markup(s)
+
+# jinja_environment.filters['urlencode'] = urlencode_filter
+
+app = Flask(__name__, static_url_path='')
+# app.wsgi_app = wrap_wsgi_app(app.wsgi_app)
+
+    
+@app.route('/<page>')
+def render(page):
+    valid_templates = ['index', 'classes', 'instructors', 'chen', 'yang', 'resources', 'czl2016']
+    template = page.lower()
+    if template in valid_templates:
+        current_year = datetime.now().year
+        return render_template(template + '.html', current_year=current_year)
+    abort(404)
+
+
+@app.route('/')
+def root():
+    return render('index')
+
+
+if __name__ == "__main__":
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    # Flask's development server will automatically serve static files in
+    # the "static" directory. See:
+    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
+    # App Engine itself will serve those files as configured in app.yaml.
+    app.run(host="127.0.0.1", port=8080, debug=True)
+
+# class CwtaPage(webapp2.RequestHandler):    
+#     def get(self, page):
+#         current_year = datetime.now().year
+#         template = jinja_environment.get_template(page + '.html')
+#         self.response.out.write(template.render(current_year=current_year))
+
+# class HomePage(CwtaPage):
+#     def get(self):
+#         super(HomePage,self).get('index')
+
+# app = webapp2.WSGIApplication([('/', HomePage),
+#                                ('/(classes|instructors|chen|yang|resources|czl2016)/?(?i)', CwtaPage)
+#                               ],
+#                               debug=True)
+
